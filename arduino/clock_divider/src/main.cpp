d07865a5cad5693d6acf15706025567b87d5f1cf
regulus33 <zacharyrowden89@gmail.com>
2023-09-03 13:15:01 +0200

    [Arduino] Basically logging to display

--- arduino/clock_divider/src/main.cpp
+++ arduino/clock_divider/src/main.cpp
@@ -14,170 +14,51 @@ second.
 
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
-/*!
-===============================SCREEN==================================================
-*/
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+#define POT_PIN A0
+#define A_CLOCK 5
+#define B_CLOCK 2
+#define C_CLOCK 3
+#define A_PUSH_BUTTON 7
+#define B_PUSH_BUTTON 11
+#define C_PUSH_BUTTON 12
+
 #define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1   //   QT-PY / XIAO
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-#define LOGO16_GLCD_HEIGHT 16
-#define LOGO16_GLCD_WIDTH  16
-#define POT_PIN A0
-#define A_CLOCK 5
-#define B_CLOCK 2
-#define C_CLOCK 3
+
 #define PUSH_BUTTON_LED 10
-#define A_PUSH_BUTTON 7
-#define B_PUSH_BUTTON 11
-#define C_PUSH_BUTTON 12
+
 
 /* init timer */
-
-int minBPM = 0;
-int maxBPM = 200;
-
-int potValue;
-int bpm = 140;
-int lastPotValue = 0;
-int pushButtonDown = false;
-
-int ppqnsArray[] = { 2, 4, 8, 12, 24, 48 };
-const int ppqnIndicesMax = (sizeof(ppqnsArray) / sizeof(ppqnsArray[0]));
-int ppqnIndices[] = {0,0,0};
-bool clockStates[] = {false,false,false};
-unsigned long previousTime[] = {0, 0, 0};
-int lastButtonState[] = {HIGH,HIGH,HIGH};
-
-// pushbuttons
-int aPushState = HIGH;
-int bPushState = HIGH;
-int cPushState = HIGH;
 
 
 
 void initDisplay() {
     display.begin(i2c_Address, true); // Address 0x3C default
-    // Clear the buffer.
     display.clearDisplay();
     display.setTextSize(2);
     display.setTextColor(SH110X_WHITE);
 }
 
-void setupClockOutputs() {
-    pinMode(A_CLOCK, OUTPUT);
-    pinMode(B_CLOCK, OUTPUT);
-    pinMode(C_CLOCK, OUTPUT);
-}
-
-bool boolToDigitalPol(bool i) {
-    if(i) {
-        return HIGH;
-    }
-    return LOW;
-}
-
-void updateClock(int clockPin, int clockIndex, unsigned long currentTime) {
-    int pulseCount = ppqnsArray[ppqnIndices[clockIndex]];
-    int beatTime = 60000 / bpm;
-    int pulseTime = (beatTime / pulseCount) / 2; // 2? TODO
-
-    if (currentTime - previousTime[clockIndex] >= pulseTime) {
-        bool pol = boolToDigitalPol(!clockStates[clockIndex]);
-        clockStates[clockIndex] = !clockStates[clockIndex];
-        digitalWrite(clockPin, pol);
-        previousTime[clockIndex] = currentTime;
-    }
-}
-
-void drawBPM() {
+void printNumber(int number) {
     display.clearDisplay(); // clear the display first
     display.setCursor(25, 25); // reset cursor position
-    display.print("BPM: ");
-    display.print(bpm);
-    display.display(); // Update the display with all the above changes
-}
-
-void drawPPQN(int clockIndex) {
-    display.clearDisplay(); // clear the display first
-    display.setCursor(25, 25); // reset cursor position
-    int ppqn = ppqnsArray[ppqnIndices[clockIndex]];
-    display.print(ppqn);
-    display.print(" PPQN");
-    display.display(); // Update the display with all the above changes
-}
-
-bool updatePushState(int &pushState, int clockIndex) {
-    if(pushState == LOW && lastButtonState[clockIndex] == HIGH) {
-
-        ppqnIndices[clockIndex] = (ppqnIndices[clockIndex] + 1) % ppqnIndicesMax;
-        drawPPQN(clockIndex);
-        return true;
-    }
-    return false;
-}
-
-void updatePushStates() {
-    aPushState = digitalRead(A_PUSH_BUTTON);
-    bPushState = digitalRead(B_PUSH_BUTTON);
-    cPushState = digitalRead(C_PUSH_BUTTON);
-
-    bool a = updatePushState(aPushState, 0);
-    bool b = updatePushState(bPushState, 1);
-    bool c = updatePushState(cPushState, 2);
-
-    lastButtonState[0] = aPushState;
-    lastButtonState[1] = bPushState;
-    lastButtonState[2] = cPushState;
-
-    if(a || b || c) {
-        pushButtonDown = true;
-    } else {
-        pushButtonDown = false;
-    }
-}
-
-
-void handlePushLED() {
-    if(pushButtonDown) {
-        digitalWrite(PUSH_BUTTON_LED, HIGH);
-    } else {
-        digitalWrite(PUSH_BUTTON_LED, LOW);
-    }
-
-}
-
-bool potChangedSignificantly(int newPotValue) {
-    int potDiff = lastPotValue - newPotValue;
-    int absPotDiff = abs(potDiff);
-    if((absPotDiff > 5)) {
-        return true;
-    }
-    return false;
+    display.print(number);
+    display.display();
 }
 
 void loop() {
-    int newPotValue = analogRead(POT_PIN);
-    // check if the potentiometer value has changed
-    if (newPotValue != lastPotValue && potChangedSignificantly(newPotValue)) {
-        bpm = map(newPotValue, 0, 1023, minBPM, maxBPM); // just make 0 - 1023 50 - 300
-        lastPotValue = newPotValue;
-        drawBPM();
-    }
-    unsigned long currentTime = millis();
-    updateClock(A_CLOCK, 0, currentTime);
-    updateClock(B_CLOCK, 1, currentTime);
-    updateClock(C_CLOCK, 2, currentTime);
-
-    updatePushStates();
-    handlePushLED();
+    int m = millis();
+    delay(333);
+    printNumber(m);
 }
 
 
@@ -185,16 +66,12 @@ void setup() {
     Serial.begin(9600);
     delay(250); // wait for the OLED to power up
     initDisplay();
-//    display.drawBitmap(25, 0, myBitmap, 128, 64, SH110X_WHITE);
     display.display();
     delay(2000);
-    setupClockOutputs();
     pinMode(PUSH_BUTTON_LED, OUTPUT);
 
     // initialize the pushbutton pins as inputs with pull-up resistors:
-    pinMode(A_PUSH_BUTTON, INPUT_PULLUP);
-    pinMode(B_PUSH_BUTTON, INPUT_PULLUP);
-    pinMode(C_PUSH_BUTTON, INPUT_PULLUP);
-
-
+    // pinMode(A_PUSH_BUTTON, INPUT_PULLUP);
+    // pinMode(B_PUSH_BUTTON, INPUT_PULLUP);
+    // pinMode(C_PUSH_BUTTON, INPUT_PULLUP);
 }

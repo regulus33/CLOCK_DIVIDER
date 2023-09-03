2b4a407c024a6247e5d65f454ae3b175d05f3a9a
regulus33 <zacharyrowden89@gmail.com>
2023-09-03 17:05:06 +0200

    [Arduino] Add broken midi output

--- arduino/clock_divider/src/main.cpp
+++ arduino/clock_divider/src/main.cpp
@@ -1,102 +1,196 @@
+/*********************************************************************
+A beat is a quarter note. 60 bpm is 60 quarter notes in a minute or
+it is 1 quarter note in a second. A quarter note is not the clock, it
+is a function of the synchornization provided by the clock. So the
+question is, what the fuck is the clock then? Well, its just pulses
+firing off rapidly. A slow square wave. Different devices use different
+resolutions but the standard is 24 pulses per quarter note. To go back
+to our example of 60 bpm. 24 ppqn would generate a pulse 24 times per
+second.
+
+*********************************************************************/
+
+
+
 #include <SPI.h>
 #include <Wire.h>
+#include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
-#include "EncoderKnobService.h"
+#include <MIDI.h>
+// TODO: this needs to be organized
+MIDI_CREATE_DEFAULT_INSTANCE();
 
-#define NUMFLAKES 10
-#define XPOS 0
-#define YPOS 1
-#define DELTAY 2
-#define POT_PIN A0
-#define A_CLOCK 5
-#define B_CLOCK 2
-#define C_CLOCK 3
-#define A_PUSH_BUTTON 7
-#define B_PUSH_BUTTON 11
-#define C_PUSH_BUTTON 12
-
-// initialize the pushbutton pins as inputs with pull-up resistors:
-// pinMode(A_PUSH_BUTTON, INPUT_PULLUP);
-// pinMode(B_PUSH_BUTTON, INPUT_PULLUP);
-// pinMode(C_PUSH_BUTTON, INPUT_PULLUP);
-
+/*!
+===============================SCREEN==================================================
+*/
 #define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1   //   QT-PY / XIAO
-#define PUSH_BUTTON_LED 10
-
-// CLASSES TODO: abstract us out of this file please
-//
-//
-//
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-EncoderKnobService encoderKnobService;
+#define POT_PIN A0
+#define A_CLOCK 5
+#define B_CLOCK 2
+#define C_CLOCK 3
+#define PUSH_BUTTON_LED 10
+#define A_PUSH_BUTTON 7
+#define B_PUSH_BUTTON 11
+#define C_PUSH_BUTTON 12
 
-//// END CLASSES
+int minBPM = 0;
+int maxBPM = 200;
+int bpm = 140;
+int lastPotValue = 0;
+int pushButtonDown = false;
+int ppqnsArray[] = {2, 4, 8, 12, 24, 48};
+const int ppqnIndicesMax = (sizeof(ppqnsArray) / sizeof(ppqnsArray[0]));
+int ppqnIndices[] = {0, 0, 0};
+bool clockStates[] = {false, false, false};
+unsigned long previousTime[] = {0, 0, 0};
+int lastButtonState[] = {HIGH, HIGH, HIGH};
+/*! pushbuttons */
+int aPushState = HIGH;
+int bPushState = HIGH;
+int cPushState = HIGH;
+/*! MIDI LIGHT */
+bool midiLight = false;
 
+void handlePushLED() {
+    if (pushButtonDown) {
+        digitalWrite(PUSH_BUTTON_LED, HIGH);
+    } else {
+        digitalWrite(PUSH_BUTTON_LED, LOW);
+    }
+}
 
+bool boolToDigitalPol(bool i) {
+    if (i) {
+        return HIGH;
+    }
+    return LOW;
+}
 
-
-/*! vars TODO: fix me, no global vars please */
-volatile unsigned long ticks = 0;
+void drawPPQN(int clockIndex) {
+    display.clearDisplay(); // clear the display first
+    display.setCursor(25, 25); // reset cursor position
+    int ppqn = ppqnsArray[ppqnIndices[clockIndex]];
+    display.print(ppqn);
+    display.print(" PPQN");
+    display.display(); // Update the display with all the above changes
+}
 
 void initDisplay() {
-    Serial.begin(9600);
     display.begin(i2c_Address, true); // Address 0x3C default
+    // Clear the buffer.
     display.clearDisplay();
     display.setTextSize(2);
     display.setTextColor(SH110X_WHITE);
-    delay(250); // wait for the OLED to power up
 }
 
-void printNumber(long number) {
+void setupClockOutputs() {
+    pinMode(A_CLOCK, OUTPUT);
+    pinMode(B_CLOCK, OUTPUT);
+    pinMode(C_CLOCK, OUTPUT);
+}
+
+void updateClock(int clockPin, int clockIndex, unsigned long currentTime) {
+    int pulseCount = ppqnsArray[ppqnIndices[clockIndex]];
+    int beatTime = 60000 / bpm;
+    int pulseTime = (beatTime / pulseCount) / 2; // 2? TODO
+
+    if (currentTime - previousTime[clockIndex] >= pulseTime) {
+        if(clockPin == A_CLOCK) {
+            MIDI.sendRealTime(midi::Clock);
+            int lightState = !midiLight;
+            digitalWrite(3, lightState);
+        }
+        bool pol = boolToDigitalPol(!clockStates[clockIndex]);
+        clockStates[clockIndex] = !clockStates[clockIndex];
+        digitalWrite(clockPin, pol);
+        previousTime[clockIndex] = currentTime;
+    }
+}
+
+bool updatePushState(int &pushState, int clockIndex) {
+    if (pushState == LOW && lastButtonState[clockIndex] == HIGH) {
+
+        ppqnIndices[clockIndex] = (ppqnIndices[clockIndex] + 1) % ppqnIndicesMax;
+        drawPPQN(clockIndex);
+        return true;
+    }
+    return false;
+}
+
+void drawBPM() {
     display.clearDisplay(); // clear the display first
     display.setCursor(25, 25); // reset cursor position
-    display.print(number);
-    display.display();
+    display.print("BPM: ");
+    display.print(bpm);
+    display.display(); // Update the display with all the above changes
 }
 
-void initTimerInterrupts() {
-    noInterrupts(); // We need to disable them first i guess?
-    TCCR1A = 0;
-    TCCR1B = 0;
-    // Set compare match register to desired timer count
-    // (16,000,000 / (prescaler * desired interrupt frequency))
-    OCR1A = 15624;            // Compare match register for 1Hz output
-    // Enable Timer1 interrupt
-    TIMSK1 |= (1 << OCIE1A);  // Enable timer compare interrupt
-    // Initialize Timer1
-    // WGM12 = CTC Mode, CS12 = 256 prescaler
-    TCCR1B |= (1 << WGM12) | (1 << CS12);
-    interrupts();             // Enable all interrupts
+void updatePushStates() {
+    aPushState = digitalRead(A_PUSH_BUTTON);
+    bPushState = digitalRead(B_PUSH_BUTTON);
+    cPushState = digitalRead(C_PUSH_BUTTON);
+
+    bool a = updatePushState(aPushState, 0);
+    bool b = updatePushState(bPushState, 1);
+    bool c = updatePushState(cPushState, 2);
+
+    lastButtonState[0] = aPushState;
+    lastButtonState[1] = bPushState;
+    lastButtonState[2] = cPushState;
+
+    if (a || b || c) {
+        pushButtonDown = true;
+    } else {
+        pushButtonDown = false;
+    }
 }
+
+bool potChangedSignificantly(int newPotValue) {
+    int potDiff = lastPotValue - newPotValue;
+    int absPotDiff = abs(potDiff);
+    if ((absPotDiff > 5)) {
+        return true;
+    }
+    return false;
+}
+
 
 void setup() {
-    // Literal push buttons
-//    pinMode(A_PUSH_BUTTON, INPUT_PULLUP);
-//    pinMode(B_PUSH_BUTTON, INPUT_PULLUP);
-//    pinMode(C_PUSH_BUTTON, INPUT_PULLUP);
-
-    // Display stuff
+//    Serial.begin(9600);
+    delay(250); // wait for the OLED to power up
     initDisplay();
     display.display();
-}
+    setupClockOutputs();
+    pinMode(PUSH_BUTTON_LED, OUTPUT);
 
-ISR(TIMER1_COMPA_vect) {
-    // This ISR is called every second
-    ticks++;
-//    int tickss = ticks;
-//    Serial.println(ticks);
-//    printNumber(tickss);
-}
+    // initialize the pushbutton pins as inputs with pull-up resistors:
+    pinMode(A_PUSH_BUTTON, INPUT_PULLUP);
+    pinMode(B_PUSH_BUTTON, INPUT_PULLUP);
+    pinMode(C_PUSH_BUTTON, INPUT_PULLUP);
 
+    // ... your existing setup code
+    MIDI.begin(MIDI_CHANNEL_OMNI);  // listen on all channels
+    Serial.begin(31250);
+}
 
 
 void loop() {
-//    if(encoderKnobService.valueChanged()) {
-//        Serial.println(encoderKnobService.getCurrentPosition());
-//        printNumber(encoderKnobService.getCurrentPosition());
-//    }
-    printNumber(encoderKnobService.getCurrentPosition());
+    int newPotValue = analogRead(POT_PIN);
+    // check if the potentiometer value has changed
+    if (newPotValue != lastPotValue && potChangedSignificantly(newPotValue)) {
+        bpm = map(newPotValue, 0, 1023, minBPM, maxBPM); // just make 0 - 1023 50 - 300
+        lastPotValue = newPotValue;
+        drawBPM();
+    }
+    unsigned long currentTime = millis();
+    updateClock(A_CLOCK, 0, currentTime);
+    updateClock(B_CLOCK, 1, currentTime);
+    updateClock(C_CLOCK, 2, currentTime);
+
+    updatePushStates();
+    handlePushLED();
 }

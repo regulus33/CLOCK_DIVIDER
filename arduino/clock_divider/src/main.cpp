4cb01f0ac9ef68d308c8cea2b9983d859952bb42
regulus33 <zacharyrowden89@gmail.com>
2023-09-03 12:59:19 +0200

    Old working main

--- arduino/clock_divider/src/main.cpp
+++ arduino/clock_divider/src/main.cpp
@@ -1,45 +1,200 @@
-#include <Arduino.h>
-#include "ArduinoClawk.h"
-#include "OledDisplayService.h"
-/*
-  Blink
+/*********************************************************************
+A beat is a quarter note. 60 bpm is 60 quarter notes in a minute or
+it is 1 quarter note in a second. A quarter note is not the clock, it
+is a function of the synchornization provided by the clock. So the
+question is, what the fuck is the clock then? Well, its just pulses
+firing off rapidly. A slow square wave. Different devices use different
+resolutions but the standard is 24 pulses per quarter note. To go back
+to our example of 60 bpm. 24 ppqn would generate a pulse 24 times per
+second.
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+*********************************************************************/
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
 
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+/*!
+===============================SCREEN==================================================
 */
-Clawk c = Clawk(1);
-OledDisplayService display;
-// the setup function runs once when you press reset or power the board
-void setup() {
-    display.displayText("Hello world!", 0, 0);
-    // initialize digital pin LED_BUILTIN as an output.
-    pinMode(LED_BUILTIN, OUTPUT);
+#define i2c_Address 0x3c //initialize with the I2C addr 0x3C Typically eBay OLED's
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1   //   QT-PY / XIAO
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+#define LOGO16_GLCD_HEIGHT 16
+#define LOGO16_GLCD_WIDTH  16
+#define POT_PIN A0
+#define A_CLOCK 5
+#define B_CLOCK 2
+#define C_CLOCK 3
+#define PUSH_BUTTON_LED 10
+#define A_PUSH_BUTTON 7
+#define B_PUSH_BUTTON 11
+#define C_PUSH_BUTTON 12
+
+/* init timer */
+
+int minBPM = 0;
+int maxBPM = 200;
+
+int potValue;
+int bpm = 140;
+int lastPotValue = 0;
+int pushButtonDown = false;
+
+int ppqnsArray[] = { 2, 4, 8, 12, 24, 48 };
+const int ppqnIndicesMax = (sizeof(ppqnsArray) / sizeof(ppqnsArray[0]));
+int ppqnIndices[] = {0,0,0};
+bool clockStates[] = {false,false,false};
+unsigned long previousTime[] = {0, 0, 0};
+int lastButtonState[] = {HIGH,HIGH,HIGH};
+
+// pushbuttons
+int aPushState = HIGH;
+int bPushState = HIGH;
+int cPushState = HIGH;
+
+
+
+void initDisplay() {
+    display.begin(i2c_Address, true); // Address 0x3C default
+    // Clear the buffer.
+    display.clearDisplay();
+    display.setTextSize(2);
+    display.setTextColor(SH110X_WHITE);
 }
 
-// the loop function runs over and over again forever
+void setupClockOutputs() {
+    pinMode(A_CLOCK, OUTPUT);
+    pinMode(B_CLOCK, OUTPUT);
+    pinMode(C_CLOCK, OUTPUT);
+}
+
+bool boolToDigitalPol(bool i) {
+    if(i) {
+        return HIGH;
+    }
+    return LOW;
+}
+
+void updateClock(int clockPin, int clockIndex, unsigned long currentTime) {
+    int pulseCount = ppqnsArray[ppqnIndices[clockIndex]];
+    int beatTime = 60000 / bpm;
+    int pulseTime = (beatTime / pulseCount) / 2; // 2? TODO
+
+    if (currentTime - previousTime[clockIndex] >= pulseTime) {
+        bool pol = boolToDigitalPol(!clockStates[clockIndex]);
+        clockStates[clockIndex] = !clockStates[clockIndex];
+        digitalWrite(clockPin, pol);
+        previousTime[clockIndex] = currentTime;
+    }
+}
+
+void drawBPM() {
+    display.clearDisplay(); // clear the display first
+    display.setCursor(25, 25); // reset cursor position
+    display.print("BPM: ");
+    display.print(bpm);
+    display.display(); // Update the display with all the above changes
+}
+
+void drawPPQN(int clockIndex) {
+    display.clearDisplay(); // clear the display first
+    display.setCursor(25, 25); // reset cursor position
+    int ppqn = ppqnsArray[ppqnIndices[clockIndex]];
+    display.print(ppqn);
+    display.print(" PPQN");
+    display.display(); // Update the display with all the above changes
+}
+
+bool updatePushState(int &pushState, int clockIndex) {
+    if(pushState == LOW && lastButtonState[clockIndex] == HIGH) {
+
+        ppqnIndices[clockIndex] = (ppqnIndices[clockIndex] + 1) % ppqnIndicesMax;
+        drawPPQN(clockIndex);
+        return true;
+    }
+    return false;
+}
+
+void updatePushStates() {
+    aPushState = digitalRead(A_PUSH_BUTTON);
+    bPushState = digitalRead(B_PUSH_BUTTON);
+    cPushState = digitalRead(C_PUSH_BUTTON);
+
+    bool a = updatePushState(aPushState, 0);
+    bool b = updatePushState(bPushState, 1);
+    bool c = updatePushState(cPushState, 2);
+
+    lastButtonState[0] = aPushState;
+    lastButtonState[1] = bPushState;
+    lastButtonState[2] = cPushState;
+
+    if(a || b || c) {
+        pushButtonDown = true;
+    } else {
+        pushButtonDown = false;
+    }
+}
+
+
+void handlePushLED() {
+    if(pushButtonDown) {
+        digitalWrite(PUSH_BUTTON_LED, HIGH);
+    } else {
+        digitalWrite(PUSH_BUTTON_LED, LOW);
+    }
+
+}
+
+bool potChangedSignificantly(int newPotValue) {
+    int potDiff = lastPotValue - newPotValue;
+    int absPotDiff = abs(potDiff);
+    if((absPotDiff > 5)) {
+        return true;
+    }
+    return false;
+}
+
 void loop() {
-    c.tick();
-    digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-    delay(500);
-    display.displayText("Hello world!", 0, 0);// wait for a second
-    digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-    delay(500);                      // wait for a second
-    display.displayText("Hello back!", 0, 0);// wait for a second
+    int newPotValue = analogRead(POT_PIN);
+    // check if the potentiometer value has changed
+    if (newPotValue != lastPotValue && potChangedSignificantly(newPotValue)) {
+        bpm = map(newPotValue, 0, 1023, minBPM, maxBPM); // just make 0 - 1023 50 - 300
+        lastPotValue = newPotValue;
+        drawBPM();
+    }
+    unsigned long currentTime = millis();
+    updateClock(A_CLOCK, 0, currentTime);
+    updateClock(B_CLOCK, 1, currentTime);
+    updateClock(C_CLOCK, 2, currentTime);
+
+    updatePushStates();
+    handlePushLED();
+}
+
+
+void setup() {
+    Serial.begin(9600);
+    delay(250); // wait for the OLED to power up
+    initDisplay();
+//    display.drawBitmap(25, 0, myBitmap, 128, 64, SH110X_WHITE);
+    display.display();
+    delay(2000);
+    setupClockOutputs();
+    pinMode(PUSH_BUTTON_LED, OUTPUT);
+
+    // initialize the pushbutton pins as inputs with pull-up resistors:
+    pinMode(A_PUSH_BUTTON, INPUT_PULLUP);
+    pinMode(B_PUSH_BUTTON, INPUT_PULLUP);
+    pinMode(C_PUSH_BUTTON, INPUT_PULLUP);
+
+
 }

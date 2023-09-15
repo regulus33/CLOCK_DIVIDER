75fd7ba67d23fb7ae98b7397bf5ba312919b1798
regulus33 <zacharyrowden89@gmail.com>
2023-09-15 22:27:29 +0200

    [Arduino] divider test

--- arduino/clock_divider/include/state/Divider.h
+++ arduino/clock_divider/include/state/Divider.h
@@ -5,25 +5,23 @@
 #ifndef CLOCK_DIVIDER_DIVIDER_H
 #define CLOCK_DIVIDER_DIVIDER_H
 
+
 class Divider {
 public:
-    int counter;
-    int counterMax = 8;
-    int currentDivisionIndex = 0;
-    const int divisions[8] = {1,2,3,4,5,6,7, 8};
+    int counter = 0;
+    int counterMax = 3;
 
-    Divider(int division) {
-        currentDivisionIndex = (division - 1);
-    }
-    bool tick() {
-        int tick = false;
-        if(counter % divisions[currentDivisionIndex] == 0) {
-            tick = true;
+    Divider(int division) { counterMax = division; }
+
+    int tick() {
+        int tick = 0;
+        if (counter % counterMax == 0) {
+            tick = 1;
         }
         counter = (counter + 1) % counterMax;
         return tick;
     }
-
 };
+
 
 #endif //CLOCK_DIVIDER_DIVIDER_H
